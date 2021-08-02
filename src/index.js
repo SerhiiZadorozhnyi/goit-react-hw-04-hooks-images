e3a855d87839554a890b83526bef7d14d75b83db
SerhiiZadorozhnyi <71183966+SerhiiZadorozhnyi@users.noreply.github.com>
2021-08-02 21:54:24 +0200

    Fix

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'modern-normalize/modern-normalize.css';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
-
-/* eslint no-undef: "error" */
-/* eslint-env browser */
 
 ReactDOM.render(
   <React.StrictMode>
